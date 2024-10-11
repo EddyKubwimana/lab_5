8ef34ba2c884951ae516d800a0f9842a7fe5b6c6
EddyKubwimana <eddy.kubwimana@ashesi.edu.gh>
2024-10-11 16:46:18 +0000

    setting up connection

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
 import redis
+import mysql.connector
+import os
+
 
 app = Flask(__name__)
 
@@ -9,7 +12,27 @@ r = redis.Redis(host="redis", port=6379)
 @app.route("/")
 def home():
     count = r.incr("hits")
+    
     return render_template("index.html", count = count)
+
+
+
+def db_connection(host, username, password, database):
+    '''
+    - Take host
+    - take username
+    - Take password
+    - database
+
+    '''
+    connection = mysql.connector.connect(
+        host=host, 
+        user=username,
+        password=password,
+        database=database
+    )
+    return connection
+
 
 
 if __name__ == "__main__":
