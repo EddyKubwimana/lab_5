4cd9a30e90b0b59f6d1874ce1e8edae2f4b056a5
EddyKubwimana <eddy.kubwimana@ashesi.edu.gh>
2024-10-11 17:31:44 +0000

    updating the app.py and index.html

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,73 +1,59 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import redis
 import mysql.connector
 
-import os
-
-
 app = Flask(__name__)
 
+# Redis for tracking page visits
 r = redis.Redis(host="redis", port=6379)
-
 
 
 @app.route("/")
 def home():
     count = r.incr("hits")
-    conn = db_connection("mysql","EddyKubwimana","password", "cloud")
+    conn = db_connection("mysql", "EddyKubwimana", "password", "cloud")
     cursor = conn.cursor()
 
+    # Create the table if not exists
     cursor.execute('''
-        CREATE TABLE IF NOT EXISTS people  (
+        CREATE TABLE IF NOT EXISTS people (
         id INT AUTO_INCREMENT PRIMARY KEY,
-       firstname VARCHAR(100),
-       lastname VARCHAR(100),
-       registration_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        firstname VARCHAR(100),
+        lastname VARCHAR(100),
+        registration_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         );
     ''')
 
-
-
-
-   
-    cursor = conn.cursor(dictionary=True) 
+    cursor = conn.cursor(dictionary=True)
     cursor.execute('SELECT * FROM people ORDER BY registration_time DESC')
     people_data = cursor.fetchall()
-    print("People data : ", people_data)
-    print("Count ", count)
     cursor.close()
     conn.close()
 
+    return render_template("index.html", count=count, people=people_data)
 
-
-
-    return render_template("index.html", count = count, people = people_data )
 
 @app.route('/submit', methods=['POST'])
 def submit():
     firstname = request.form['firstname']
     lastname = request.form['lastname']
-    conn = db_connection("mysql","EddyKubwimana","password", "cloud")
+    conn = db_connection("mysql", "EddyKubwimana", "password", "cloud")
     cursor = conn.cursor()
     cursor.execute('INSERT INTO people (firstname, lastname) VALUES (%s, %s)', (firstname, lastname))
     conn.commit()
+
+    cursor.execute('SELECT * FROM people ORDER BY registration_time DESC')
+    people_data = cursor.fetchall()
     cursor.close()
     conn.close()
-    
-    return render_template('index.html', message="User added successfully!")
 
+   
+    return jsonify({'message': 'User added successfully!', 'people': people_data, 'count': r.get("hits").decode()})
 
 
 def db_connection(host, username, password, database):
-    '''
-    - Take host
-    - take username
-    - Take password
-    - database
-
-    '''
     connection = mysql.connector.connect(
-        host=host, 
+        host=host,
         user=username,
         password=password,
         database=database
@@ -75,6 +61,5 @@ def db_connection(host, username, password, database):
     return connection
 
 
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port = 5000)
+    app.run(host="0.0.0.0", port=5000)
