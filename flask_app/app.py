4484cf262b0bea60da1ad29d5cdd8230e3883882
EddyKubwimana <eddy.kubwimana@ashesi.edu.gh>
2024-10-11 17:13:16 +0000

    added mysql service

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import redis
 import mysql.connector
+
 import os
 
 
@@ -9,11 +10,49 @@ app = Flask(__name__)
 r = redis.Redis(host="redis", port=6379)
 
 
+
 @app.route("/")
 def home():
     count = r.incr("hits")
+    conn = db_connection("mysql","EddyKubwimana","password", "cloud")
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS people  (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+       firstname VARCHAR(100),
+       lastname VARCHAR(100),
+       registration_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+    ''')
+
+
+
+
+   
+    cursor = conn.cursor(dictionary=True) 
+    cursor.execute('SELECT * FROM people ORDER BY registration_time DESC')
+    people_data = cursor.fetchall()
+    cursor.close()
+    conn.close()
+
+
+
+
+    return render_template("index.html", count = count, people = people_data )
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    firstname = request.form['firstname']
+    lastname = request.form['lastname']
+    conn = db_connection("mysql","EddyKubwimana","password", "cloud")
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO people (firstname, lastname) VALUES (%s, %s)', (firstname, lastname))
+    conn.commit()
+    cursor.close()
+    conn.close()
     
-    return render_template("index.html", count = count)
+    return render_template('index.html', message="User added successfully!")
 
 
 
